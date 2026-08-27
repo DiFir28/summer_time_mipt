69c812a6deb6a9aa3b1cf68a5e8ca020b063542e
DiFir28 <28102008dima@gmail.com>
2026-08-27 12:45:24 -0200

    Add scale at mouse cursor

--- include/visual.h
+++ include/visual.h
@@ -7,6 +7,30 @@
 #define WINDOWS_SIZE 1000
 
 /**
+ * @brief struct contain linear offset and scale for coordinate system
+ */
+struct offset{
+    Vector2 coords; /**<  linear offset*/
+    Vector2 scale;  /**<  scale (how many pix for 1 sing. seg.)*/
+};
+
+/**
+ * @brief transform between 2 coordinate system (from Qadratic to Visual coords system)
+ * @param[in] target        target of transform
+ * @param[in] axis_offset   transform coefficents (offset and scale)
+ * @return transformed Vector2
+ */
+Vector2 converQtoV(Vector2 target, offset axis_offset);
+
+/**
+ * @brief transform between 2 coordinate system (from Visal to Qadratic coords system)
+ * @param[in] target        target of transform
+ * @param[in] axis_offset   transform coefficents (offset and scale)
+ * @return transformed Vector2
+ */
+Vector2 converVtoQ(Vector2 target, offset axis_offset);
+
+/**
  * @brief Init window and set frame rate
  */
 void visualInit();
--- src/visual.cpp
+++ src/visual.cpp
@@ -6,16 +6,18 @@
 #include "raylib.h"
 #include "quade.h"
 
-struct offset{
-    Vector2 coords;
-    Vector2 scale;
-};
-
 Vector2 converQtoV(Vector2 target, offset axis_offset){
     return (Vector2){
         target.x * axis_offset.scale.x + axis_offset.coords.x,
         target.y * axis_offset.scale.y + axis_offset.coords.y,};
 }
+
+Vector2 converVtoQ(Vector2 target, offset axis_offset){
+    return (Vector2){
+        (target.x - axis_offset.coords.x) / axis_offset.scale.x,
+        (target.y - axis_offset.coords.y) / axis_offset.scale.y,};
+}
+
 
 void visualInit(){
     InitWindow( WINDOWS_SIZE, WINDOWS_SIZE, "Quadratic equation");
@@ -27,40 +29,48 @@ void visualInit(){
 
 void drawMainGrid(offset axis_offset){
     char title[12] = "";
-    int line_cnt = 0;
+    
     int num_step = 1;
     if (axis_offset.scale.y < 20){
         num_step = int(40 / axis_offset.scale.y);
     }
-    for (float d = 0, pix_step = axis_offset.scale.y * num_step; d - abs(axis_offset.coords.x) <  WINDOWS_SIZE; d += pix_step, line_cnt += num_step){
-        Vector2 p1 = { 0, d + axis_offset.coords.y};
-        Vector2 p2 = {float(WINDOWS_SIZE), d + axis_offset.coords.y};
+    for (int line_cnt = 0; line_cnt * axis_offset.scale.y - abs(axis_offset.coords.y) <  WINDOWS_SIZE; line_cnt += num_step){
+        // draw and subscribe lines parallel OX with positive Y
+        Vector2 J = converQtoV((Vector2){0, float(line_cnt)}, axis_offset);
+        Vector2 p1 = {0, J.y};
+        Vector2 p2 = {float(WINDOWS_SIZE), J.y};
         DrawLineEx(p1, p2, 2, GRAY);
-        Vector2 p3 = { 0, - d + axis_offset.coords.y};
-        Vector2 p4 = {float(WINDOWS_SIZE), - d + axis_offset.coords.y};
-        DrawLineEx(p3, p4, 2, GRAY);
         sprintf(title, "%i", - line_cnt);
-        DrawText(title, axis_offset.coords.x + 3, d + axis_offset.coords.y + 5, 30, LIGHTGRAY);
+        DrawText(title, J.x + 3, J.y + 5, 30, LIGHTGRAY);
+        // draw and subscribe lines parallel OX with negative Y
+        Vector2 negJ = converQtoV((Vector2){0, float(-line_cnt)}, axis_offset);
+        Vector2 p3 = {0, negJ.y};
+        Vector2 p4 = {float(WINDOWS_SIZE), negJ.y};
+        DrawLineEx(p3, p4, 2, GRAY);
         sprintf(title, "%i", line_cnt);
-        DrawText(title, axis_offset.coords.x + 3, - d + axis_offset.coords.y + 5, 30, LIGHTGRAY);
+        DrawText(title, negJ.x + 3, negJ.y + 5, 30, LIGHTGRAY);
     }
-    line_cnt = 0;
     num_step = 1;
     if (axis_offset.scale.x < 20){
         num_step = int(40 / axis_offset.scale.x);
     }    
-    for (float d = 0, pix_step = axis_offset.scale.x * num_step; d - abs(axis_offset.coords.x) <  WINDOWS_SIZE; d += pix_step, line_cnt += num_step){
-        Vector2 p1 = { d + axis_offset.coords.x, 0};
-        Vector2 p2 = { d + axis_offset.coords.x, float(WINDOWS_SIZE)};
+    for (int line_cnt = 0; line_cnt * axis_offset.scale.x - abs(axis_offset.coords.x) <  WINDOWS_SIZE; line_cnt += num_step){
+        // draw and subscribe lines parallel OY with positive X
+        Vector2 I = converQtoV((Vector2){float(line_cnt), 0}, axis_offset);
+        Vector2 p1 = { I.x, 0};
+        Vector2 p2 = { I.x, float(WINDOWS_SIZE)};
         DrawLineEx(p1, p2, 2, GRAY);
-        Vector2 p3 = { - d + axis_offset.coords.x, 0};
-        Vector2 p4 = { - d + axis_offset.coords.x, float(WINDOWS_SIZE)};
-        DrawLineEx(p3, p4, 2, GRAY);
         sprintf(title, "%i", line_cnt);
-        DrawText(title, d + axis_offset.coords.x + 3, axis_offset.coords.y + 5, 30, LIGHTGRAY);
+        DrawText(title, I.x + 3, I.y + 5, 30, LIGHTGRAY);
+        // draw and subscribe lines parallel OY with negative X
+        Vector2 negI = converQtoV((Vector2){float(-line_cnt), 0}, axis_offset);
+        Vector2 p3 = { negI.x, 0};
+        Vector2 p4 = { negI.x, float(WINDOWS_SIZE)};
+        DrawLineEx(p3, p4, 2, GRAY);
         sprintf(title, "%i", - line_cnt);
-        DrawText(title, -d + axis_offset.coords.x + 3, axis_offset.coords.y + 5, 30, LIGHTGRAY);
+        DrawText(title, negI.x + 3, negI.y + 5, 30, LIGHTGRAY);
     }
+    //Draw OX and OY
     Vector2 p1 = { 0, axis_offset.coords.y};
     Vector2 p2 = {float(WINDOWS_SIZE), axis_offset.coords.y};
     DrawLineEx(p1, p2, 4, DARKGRAY);
@@ -75,6 +85,7 @@ void drawQuadraticEquation( QuadraticEquation *q, offset vis_offset){
         Vector2 p2 = {float(X + 1), float(vis_offset.coords.y - calcValQuadraticEquation(q, (X + 1 - vis_offset.coords.x) / vis_offset.scale.x) * vis_offset.scale.y)};
         DrawLineEx( p1, p2, 5, DARKGREEN);
     }
+    //Drawing and subscribe roots
     char title[20] = "";
     Vector2 root1 = {};
     Vector2 root2 = {};
@@ -89,11 +100,11 @@ void drawQuadraticEquation( QuadraticEquation *q, offset vis_offset){
 
     case ROOTS_TWO:
         root1 = converQtoV((Vector2){float(q->root1), 0}, vis_offset);
-        root2 = converQtoV((Vector2){float(q->root2), 0}, vis_offset);
         DrawCircle(root1.x, root1.y, 6, RED);
-        DrawCircle(root2.x, root2.y, 6, RED);
         sprintf(title, "(%g, 0)", q->root1);
         DrawText(title, root1.x - 30, root1.y - 30, 30, RED);
+        root2 = converQtoV((Vector2){float(q->root2), 0}, vis_offset);
+        DrawCircle(root2.x, root2.y, 6, RED);
         sprintf(title, "(%g, 0)", q->root2);
         DrawText(title, root2.x - 30, root2.y - 30, 30, RED);
         break;
@@ -104,6 +115,7 @@ void drawQuadraticEquation( QuadraticEquation *q, offset vis_offset){
 }
 
 void mouseHandler(offset *vis_offset){
+    //Handle LKM for offset gaphics
     static Vector2 mouse = {};
     if (IsMouseButtonPressed(0)){
         mouse.x = GetMouseX();
@@ -115,17 +127,22 @@ void mouseHandler(offset *vis_offset){
         mouse.x = GetMouseX();
         mouse.y = GetMouseY();
     }
+    
+    //Handle mouse wheel for scale gaphics
     float wheel_change = GetMouseWheelMove();
     if (wheel_change != 0.0 && (vis_offset->scale.x + wheel_change > 4) && (vis_offset->scale.y + wheel_change > 4)){
+        vis_offset->coords.x = (vis_offset->coords.x - GetMouseX()) / vis_offset->scale.x;
+        vis_offset->coords.y = (vis_offset->coords.y - GetMouseY()) / vis_offset->scale.y;
         vis_offset->scale.x += wheel_change;
         vis_offset->scale.y += wheel_change;
+        vis_offset->coords.x = vis_offset->coords.x * vis_offset->scale.x + GetMouseX();
+        vis_offset->coords.y = vis_offset->coords.y * vis_offset->scale.y + GetMouseY();
     }
 }
 
 void visualLoop(QuadraticEquation *q){
     checkLink(q);
     offset visual_offset = { {float(WINDOWS_SIZE / 2.0), float(WINDOWS_SIZE / 2.0)}, {20, 20}};
-
     if (q->a != 0){
         visual_offset.coords.x -= (-q->b / (2 * q->a)) * visual_offset.scale.x;
         visual_offset.coords.y += calcValQuadraticEquation(q, (-q->b / (2 * q->a))) * visual_offset.scale.y;
